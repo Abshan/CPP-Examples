1d0d9c547de0545255371f2d2bce51aa82b5a6ac
Mohamed Abshan <40865976+Abshan@users.noreply.github.com>
2020-10-29 23:43:33 +0530

    Added MySQL connection example code

---
+++ CPP MySQL/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <mysql/jdbc.h>
+
+using namespace std;
+
+int main(void) {
+    try {
+        sql::Driver *driver;
+        sql::Connection *con;
+        sql::Statement *stmt;
+        sql::ResultSet *res;
+
+        /* Create a connection */
+        driver = get_driver_instance();
+        con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+        /* Connect to the MySQL test database */
+        con->setSchema("test1");
+
+        stmt = con->createStatement();
+        res = stmt->executeQuery("select * from testtab;");
+        while (res->next()) {
+            cout << res->getInt("id");
+            cout << res->getString("name");
+        }
+        con->close();
+        stmt->close();
+        res->close();
+    }catch (sql::SQLException &e) {
+        cout << "Error: " << e.getErrorCode();
+    }
+
+}
